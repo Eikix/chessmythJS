70784f2585c540f76c0ecdacdcd4e220f5555f3f
Eikix <66871571+Eikix@users.noreply.github.com>
2021-10-18 11:11:05 +0200

    copied from frontend dev

--- chess/utils/helpers/exitCheckMoveList.js
+++ chess/utils/helpers/exitCheckMoveList.js
@@ -7,14 +7,7 @@ function exitCheckMoveList(
     playerColor,
     board
 ) {
-    // Artificially places black king on 3;3 to see behavior.
-    // const initTmp = board['6;10'].piece;
-    // board['6;10'].piece = null;
-    // board['3;3'].piece = initTmp;
-    // console.log(board);
-
     const exitCheckMovesArray = [];
-    console.log("Careful, you're in check!");
     const allMoves = allPossibleMovesOnBoard(
         dimension,
         mobility,
@@ -22,24 +15,21 @@ function exitCheckMoveList(
         board,
         true
     );
+    let hypotheticKingPos;
 
     const fromArray = Object.keys(allMoves);
     if (!allMoves) return null;
 
     for (from of fromArray) {
+        hypotheticKingPos = playerKing;
+
         for (move of allMoves[from].moves) {
             // Implementing "leave checked state"
 
             // Deep copy of board -- See JS meaning online https://www.javascripttutorial.net/object/3-ways-to-copy-objects-in-javascript/
             const hypotheticBoard = JSON.parse(JSON.stringify(board));
             const to = move.move;
-            let hypotheticKingPos = playerKing;
-            // console.log(from);
-            // console.log(hypotheticBoard[from]);
-            //console.log(move);
-            //console.log(from);
 
-            // Keep in memory king's position
             if (hypotheticBoard[from].piece.type === 'k') {
                 hypotheticKingPos = to;
             }
